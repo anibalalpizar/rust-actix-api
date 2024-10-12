fe037b1bd937ec25b1bc299ba09b29bdd9e0636c
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-11 19:57:03 -0600

    Add pizza model and BuyPizzaRequest struct with validation

---
+++ src/models/pizza.rs
@@ -0,0 +1,8 @@
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+#[derive(Validate, Serialize, Deserialize)]
+pub struct BuyPizzaRequest {
+    #[validate(length(min = 1, message = "Name is required"))]
+    pub pizza_name: String,
+}
