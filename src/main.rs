4f5d34928888ecc696340c8f4cd2564955b07d2e
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-13 02:23:27 -0600

    Implement pizza purchase functionality with database integration and logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::env;
+
 use actix_web::web::Data;
 use actix_web::{get, patch, post, web::Json, web::Path, App, HttpResponse, HttpServer, Responder};
+use env_logger;
 use validator::Validate;
 
 mod db;
@@ -18,12 +21,20 @@ async fn get_pizzas(db: Data<Database>) -> impl Responder {
 }
 
 #[post("/buypizza")]
-async fn buy_pizza(body: Json<BuyPizzaRequest>) -> impl Responder {
+async fn buy_pizza(body: Json<BuyPizzaRequest>, db: Data<Database>) -> impl Responder {
     let is_valid = body.validate();
     match is_valid {
         Ok(_) => {
             let pizza_name = body.pizza_name.clone();
-            HttpResponse::Ok().body(format!("Buying pizza: {}", pizza_name))
+
+            let new_pizza = db.add_pizza(pizza_name).await;
+
+            match new_pizza {
+                Some(pizza) => {
+                    HttpResponse::Ok().body(format!("Bought pizza: {}", pizza.pizza_name))
+                }
+                None => HttpResponse::Ok().body("Failed to buy pizza"),
+            }
         }
         Err(e) => HttpResponse::BadRequest().body(format!("Validation error: {}", e)),
     }
@@ -37,6 +48,8 @@ async fn update_pizza(params: Path<UpdatePizzaRequest>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
     let _db = Database::init()
         .await
         .expect("Failed to initialize database");
